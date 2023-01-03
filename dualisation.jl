402c2f171b619415b451b5692cd243aaa229a2da
HannesVanOverloop <hannes.van-overloop@eleves.enpc.fr>
2023-01-03 19:20:17 +0100

    dd

--- dualisation.jl
+++ dualisation.jl
@@ -24,7 +24,7 @@ function master_pb(inputFile::String)
     @variable(master, gamma[k in 1:K, v in 1:n] >= 0)
 
 
-    @constraint(master, [e in 1:m], alpha + beta[e] >= x[e]*(lh[node1(e,n)]+lh[node2(e,n)]))
+    @constraint(master, [e in 1:m; node1(e,n)!=node2(e,n)], alpha + beta[e] >= x[e]*(lh[node1(e,n)]+lh[node2(e,n)]))
     @constraint(master, [v in 1:n], sum(y[k, v] for k in 1:K) == 1)
     @constraint(master, [e in 1:m, k in 1:K], y[k, node2(e,n)] + y[k, node1(e,n)] - 1 <= z[k, e])
     @constraint(master, [e in 1:m, k in 1:K], (y[k, node2(e,n)] + y[k, node1(e,n)]) / 2 >= z[k, e])
@@ -39,13 +39,13 @@ function master_pb(inputFile::String)
     @objective(master, Min, sum(x[e]*l[e]/2 for e in 1:m) + L*alpha + 3*sum(beta[e] for e in 1:m))
 
     # Désactive le presolve (simplification automatique du modèle)
-    set_optimizer_attribute(master, "CPXPARAM_Preprocessing_Presolve", 0)
+    # set_optimizer_attribute(master, "CPXPARAM_Preprocessing_Presolve", 0)
     # Désactive la génération de coupes automatiques
-    set_optimizer_attribute(master, "CPXPARAM_MIP_Limits_CutsFactor", 0)
+    # set_optimizer_attribute(master, "CPXPARAM_MIP_Limits_CutsFactor", 0)
     # Désactive la génération de solutions entières à partir de solutions fractionnaires
-    set_optimizer_attribute(master, "CPXPARAM_MIP_Strategy_FPHeur", -1)
+    # set_optimizer_attribute(master, "CPXPARAM_MIP_Strategy_FPHeur", -1)
     # Désactive les sorties de CPLEX (optionnel)
-    set_optimizer_attribute(master, "CPX_PARAM_SCRIND", 0)
+    # set_optimizer_attribute(master, "CPX_PARAM_SCRIND", 0)
 
     start = time()
     optimize!(master)
@@ -88,13 +88,13 @@ function slave_pb(k::Int, n::Int, y, W::Int, W_v, w_v)
     @objective(slave, Min, W*zeta + sum(W_v[v]*gamma[v] for v in 1:n))
 
     # Désactive le presolve (simplification automatique du modèle)
-    set_optimizer_attribute(slave, "CPXPARAM_Preprocessing_Presolve", 0)
+    # set_optimizer_attribute(slave, "CPXPARAM_Preprocessing_Presolve", 0)
     # Désactive la génération de coupes automatiques
-    set_optimizer_attribute(slave, "CPXPARAM_MIP_Limits_CutsFactor", 0)
+    # set_optimizer_attribute(slave, "CPXPARAM_MIP_Limits_CutsFactor", 0)
     # Désactive la génération de solutions entières à partir de solutions fractionnaires
-    set_optimizer_attribute(slave, "CPXPARAM_MIP_Strategy_FPHeur", -1)
+    # set_optimizer_attribute(slave, "CPXPARAM_MIP_Strategy_FPHeur", -1)
     # Désactive les sorties de CPLEX (optionnel)
-    set_optimizer_attribute(slave, "CPX_PARAM_SCRIND", 0)
+    # set_optimizer_attribute(slave, "CPX_PARAM_SCRIND", 0)
 
     # start = time()
     # optimize!(slave)
@@ -108,47 +108,7 @@ function slave_pb(k::Int, n::Int, y, W::Int, W_v, w_v)
     return obj# , computation_time
 end
 
-function bugFinder(inputFile::String, x_star)
-    include(inputFile)          # contains n, coordinates, lh[v], L, w_v, W_v, W, K, B
-    m = n*n
-    l = generate_l(coordinates, n)
-
-    master = Model(CPLEX.Optimizer)
-
-    @variable(master, alpha >= 0)
-    @variable(master, beta[e in 1:m] >= 0)
-
-    @constraint(master, [e in 1:m], alpha + beta[e] >= x_star[e]*(lh[node1(e,n)]+lh[node2(e,n)]))
-
-    @objective(master, Min, sum(x_star[e]*l[e]/2 for e in 1:m) + L*alpha + 3*sum(beta[e] for e in 1:m))
-
-    # Désactive le presolve (simplification automatique du modèle)
-    set_optimizer_attribute(master, "CPXPARAM_Preprocessing_Presolve", 0)
-    # Désactive la génération de coupes automatiques
-    set_optimizer_attribute(master, "CPXPARAM_MIP_Limits_CutsFactor", 0)
-    # Désactive la génération de solutions entières à partir de solutions fractionnaires
-    set_optimizer_attribute(master, "CPXPARAM_MIP_Strategy_FPHeur", -1)
-    # Désactive les sorties de CPLEX (optionnel)
-    set_optimizer_attribute(master, "CPX_PARAM_SCRIND", 0)
-
-    # start = time()
-    optimize!(master)
-    # computation_time = time() - start
-
-    feasiblefound = primal_status(master) == MOI.FEASIBLE_POINT
-    if feasiblefound
-        obj = JuMP.objective_value(master)
-        a = JuMP.value.(alpha)
-        b = JuMP.value.(beta)
-    end
-
-    return a, b, obj
-end
-
-xStar, clustersTest, test, testCompTime = master_pb("data/10_ulysses_3.tsp")
+xStar, clustersTest, test, testCompTime = master_pb("data/14_burma_3.tsp")
 println("attained value: ", test)
+println("time needed: ", testCompTime)
 println("clusters: ", clustersTest)
-alpha, beta, testObj = bugFinder("data/10_ulysses_3.tsp", xStar)
-println("alpha: ", alpha)
-println("beta: ", beta)
-println("value: ", testObj)
