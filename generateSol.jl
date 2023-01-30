36fd1910d977986a260edfb90b15b0347dcf3d5f
HannesVanOverloop <hannes.van-overloop@eleves.enpc.fr>
2023-01-30 15:19:20 +0100

    1ere sol avec cplex et recherche par chgt simple de cluster

--- generateSol.jl
+++ generateSol.jl
@@ -1,55 +1,83 @@
 using JuMP
 using CPLEX
+using Random
 
-include("common.jl")
 include("neighborhood.jl")
+
+function genSolWithSolver(n, m, K, B, L, l, lh, w_v, W_v, W)
+    getSol = Model(CPLEX.Optimizer)
+
+    @variable(getSol, x[e in 1:m], Int)
+    @variable(getSol, z[k in 1:K, e in 1:m], Bin)
+    @variable(getSol, y[k in 1:K, v in 1:n], Bin)
+    @variable(getSol, alpha >= 0)
+    @variable(getSol, beta[e in 1:m] >= 0)
+    # from the slave problem
+    @variable(getSol, zeta[k in 1:K] >= 0)
+    @variable(getSol, gamma[k in 1:K, v in 1:n] >= 0)
+
+    @constraint(getSol, [e in 1:m; node1(e,n)!=node2(e,n)], alpha + beta[e] >= x[e]*(lh[node1(e,n)]+lh[node2(e,n)]))
+    @constraint(getSol, [v in 1:n], sum(y[k, v] for k in 1:K) == 1)
+    @constraint(getSol, [e in 1:m, k in 1:K], y[k, node2(e,n)] + y[k, node1(e,n)] - 1 <= z[k, e])
+    @constraint(getSol, [e in 1:m, k in 1:K], (y[k, node2(e,n)] + y[k, node1(e,n)]) / 2 >= z[k, e])
+    @constraint(getSol, [e in 1:m], x[e] == sum(z[k, e] for k in 1:K))
+    # from the slave problem
+    @constraint(getSol, [k in 1:K, v in 1:n], zeta[k] + gamma[k, v] >= w_v[v]*y[k, v])
+    @constraint(getSol, [k in 1:K], W*zeta[k] + sum(W_v[v]*gamma[k, v] for v in 1:n) <= B - sum(w_v[v]*y[k, v] for v in 1:n))
+
+    @objective(getSol, Min, 0)
+
+    # Désactive les sorties de CPLEX
+    set_optimizer_attribute(getSol, "CPX_PARAM_SCRIND", 0)
+    optimize!(getSol)
+
+    feasiblefound = primal_status(getSol) == MOI.FEASIBLE_POINT
+    if feasiblefound
+        clusters = JuMP.value.(y)
+    end
+
+    sol = zeros(Int64, n)
+    for v in 1:n
+        for k in 1:K
+            if clusters[k,v]==true
+                sol[v] = k
+            end
+        end
+    end
+
+    return sol
+end
 
 function xFromPartition(vectorPartition, n::Int64, m::Int64)::Vector{Bool}
     # returns a vector of bools with true if the edge is in a cluster
     return Vector([(vectorPartition[node1(e, n)] == vectorPartition[node2(e, n)]) for e in 1:m])
 end
 
-function partitionValue(vectorPartition, n::Int64, m::Int64, l, lh, L::Int64)::Float64
-    # returns the value of the given partition
-    x = xFromPartition(vectorPartition, n, m)
-
-    solVal = Model(CPLEX.Optimizer)
-    @variable(solVal, delta1[e in 1:m] >= 0)
-
-    @constraint(solVal, sum(delta1[e] for e in 1:m) <= L)
-    @constraint(solVal, [e in 1:m], delta1[e] <= 3)
-
-    @objective(solVal, Max, sum(x[e]*(1/2*l[e]+delta1[e]*(lh[node1(e, n)]+lh[node2(e, n)])) for e in 1:m))
-    optimize!(solVal)
-
-    feasiblefound = primal_status(solVal) == MOI.FEASIBLE_POINT
-    if feasiblefound
-        value = JuMP.objective_value(solVal)
+function sol2Dto1D(sol2D, K::Int64, n::Int64)
+    # Input: sol is a 2D vector : sol[k][i] tells if node i is in cluster k
+    # Output: vectorSol is a 1D vector : vectorSol[i] tells in which cluster node i is
+    sol1D = zeros(Int64, n)
+    for k in 1:K
+        for i in 1:n
+            if sol2D[k][i]
+                sol1D[i] = k
+            end
+        end
     end
-
-    return value
+    return sol1D
 end
 
-function clusterValue(k::Int64, partition, n::Int64, m::Int64, w_v, W_v, W::Int64)
-    # rerturns the value of cluster k in the given partition
-    clusVal = Model(CPLEX.Optimizer)
-    @variable(clusVal, delta2[v in 1:n] >= 0)
-
-    @constraint(clusVal, sum(delta2[v] for v in 1:n) <= W)
-    @constraint(clusVal, [v in 1:n], delta2[v] <= W_v[v])
-
-    @objective(clusVal, Max, sum(w_v[v]*partition[k][v]*(1+delta2[v]) for v in 1:n))
-    optimize!(clusVal)
-
-    feasiblefound = primal_status(clusVal) == MOI.FEASIBLE_POINT
-    if feasiblefound
-        delta2Star = JuMP.value.(delta2)
-        clusterVal = JuMP.objective_value(clusVal)
+function sol1Dto2D(sol1D::Array{Int64}, K::Int64, n::Int64)
+    # Input: sol is a 1D vector : sol[i]=k if node i is in cluster k
+    # Output: sol2D is a 2D vector : sol2D[k][i] true if node i in cluster k
+    sol2D = [zeros(Bool, n) for k in 1:K]
+    for v in 1:n
+        sol2D[sol1D[v]][v] = true
     end
-
-    return clusterVal, delta2Star
+    return sol2D
 end
 
+### TO GENERATE A RANDOM SOLUTION (DOES NOT ALWAYS SUCCEED)
 function genRandomSol(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w_v, W_v, W::Int64)
     # returns a randomly generated admissible solution
     sol = [[false for i in 1:n] for k in 1:K]
@@ -59,6 +87,8 @@ function genRandomSol(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w
         sol[k][nodes[idx]] = true
         deleteat!(nodes, idx)
     end
+    println("after first round: ", sol)
+
     iter = 0
     delta2Star = nothing
     while length(nodes)>0 && iter < n+2*K
@@ -66,6 +96,7 @@ function genRandomSol(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w
         clusterIdx = rand(1:K)
         # taking a random node and trying to add it to the cluster
         idx = rand(1:length(nodes))
+        println("trying node : ", nodes[idx])
         sol[clusterIdx][nodes[idx]] = true
         # testing the value of the cluster
         clusterVal, delta2Star = clusterValue(clusterIdx, sol, n, m, w_v, W_v, W)
@@ -76,7 +107,144 @@ function genRandomSol(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w
         end
         iter += 1
     end
-    return sol, delta2Star
+
+    if length(nodes)==0
+        println("STOP bcs : all nodes dispatched")
+        solFound = true
+    else
+        println("STOP bcs : too many iterations")
+        solFound = false
+    end
+    return solFound, sol, delta2Star
+end
+
+function affectNodes(sol, nodes::Array{Int64}, iterLimit::Float64, k::Int64, direction::Int64, n::Int64, m::Int64, w_v, W_v, W::Int64, B::Int64)
+    iter = 0
+    while isempty(nodes)==false && iter < iterLimit
+        # taking the first remaining node, to add it to the cluster
+        sol[k][nodes[1]] = true
+        # testing the value of the cluster
+        clusterVal, delta2Star = clusterValue(k, sol, n, m, w_v, W_v, W)
+        if clusterVal <= B
+            deleteat!(nodes, 1)
+        else
+            sol[k][nodes[1]] = false
+            buffer = nodes[1]
+            deleteat!(nodes, 1)
+            push!(nodes, buffer)
+        end
+        # updating cluster to extend
+        k += direction
+        if k==1 || k==K
+            direction *= -1
+        end
+        iter += 1
+    end
+end
+
+function affectFirstNodes(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w_v, W_v, W::Int64)
+    sol = [[false for i in 1:n] for k in 1:K]
+    nodes = [i for i in 1:n]
+    sort!(nodes, by=x->w_v[x], rev=true) # nodes ordered by descending static weight
+    println("nodes: ", nodes)
+    # a heavy node per cluster
+    k = 1
+    direction = 1
+    iter = 0
+    while iter < K
+        sol[k][nodes[1]] = true
+        deleteat!(nodes, 1)
+        k += direction
+        if k==1 || k==K
+            direction *= -1
+        end
+        iter += 1
+    end
+
+    # affect other nodes if possible
+    reverse!(nodes) # nodes ordered by ascending static weight
+    affectNodes(sol, nodes, n*11/10, k, direction, n, m, w_v, W_v, W, B)
+
+    # check if all nodes are affected
+    if isempty(nodes)
+        println("ALL NODES DISPATCHED")
+    else
+        println("TOO MANY ITERATIONS")
+    end
+    return isempty(nodes), sol, nodes
+end
+
+function affectNode(nodeIdx::Int64, sol, K::Int64, B::Int64, n::Int64, m::Int64, w_v, W_v, W::Int64)
+    k = 1 # cluster to put the node in
+    goOn = true
+    while k<=K && goOn
+        # pick the node to move
+        nodesInCluster = Vector{Int64}()
+        for idx in 1:n
+            if sol[k][idx]
+                append!(nodesInCluster, idx)
+            end
+        end
+        nodeToMove = argmax(x->w_v[x], nodesInCluster)
+        clusterToTest = mod(k+1, K) # cluster to move nodeToMove to
+        if clusterToTest==0
+            clusterToTest = K
+        end
+        # test where to move it to
+        possibleSol = copy(sol)
+        tryCluster = true
+        while clusterToTest!=k && tryCluster
+            possibleSol[clusterToTest][nodeToMove] = true
+            movedValue, delta2 = clusterValue(clusterToTest, possibleSol, n, m, w_v, W_v, W)
+            if movedValue <= B
+                possibleSol[k][nodeToMove] = false
+                possibleSol[k][nodeIdx] = true
+                placedValue, delta2 = clusterValue(k, possibleSol, n, m, w_v, W_v, W)
+                if placedValue <= B
+                    tryCluster = false
+                    goOn = false
+                else
+                    possibleSol[k][nodeToMove] = true
+                    possibleSol[k][nodeIdx] = false
+                end
+            else
+                possibleSol[clusterToTest][nodeToMove] = false
+            end
+            # try next cluster
+            clusterToTest = mod(clusterToTest+1, K)
+            if clusterToTest==0
+                clusterToTest = K
+            end
+        end
+        k += 1
+    end
+    return goOn
+end
+
+function affectRemainingNodes(fullSol::Bool, remainingNodes::Array{Int64}, sol, K::Int64, B::Int64, n::Int64, m::Int64, w_v, W_v, W::Int64)::Bool
+    success = true
+    if fullSol==false
+        for nodeIdx in remainingNodes
+            println("TRYING TO PLACE NODE : ", nodeIdx)
+            notPlaced = affectNode(nodeIdx, sol, K, B, n, m, w_v, W_v, W)
+            if notPlaced==false
+                println(nodeIdx, " placed")
+            else
+                success = false
+            end
+        end
+    end
+    return success
+end
+
+### TO GENERATE A SOLUTION BASED ON THE STATIC NODE WEIGHTS (DOES NOT ALWAYS SUCCEED)
+function genSolByWeight(n::Int64, m::Int64, K::Int64, B::Int64, L::Int64, l, lh, w_v, W_v, W::Int64)
+    println("dispatching nodes")
+    fullSol, sol, remainingNodes = affectFirstNodes(n, m, K, B, L, l, lh, w_v, W_v, W)
+    println("placing remaining nodes: ", remainingNodes)
+    solFound = affectRemainingNodes(fullSol, remainingNodes, sol, K, B, n, m, w_v, W_v, W)
+    println("SOLUTION: ", sol2Dto1D(sol))
+    return solFound, sol
 end
 
 function couplesWithSameW2(w_v2)
@@ -97,23 +265,6 @@ function couplesWithSameW2(w_v2)
     return couples
 end
 
-function sol2Dto1D(sol)
-    # Input: sol is a 2D vector : sol[k][i] tells if node i is in cluster k
-    # Output: vectorSol is a 1D vector : vectorSol[i] tells in which cluster node i is
-    K = length(sol)
-    n = length(sol[1])
-    vectorSol = [0 for i in 1:n]
-    for k in 1:K
-        for i in 1:n
-            if sol[k][i]
-                vectorSol[i] = k
-            end
-        end
-    end
-    return vectorSol
-end
-
-
 function heuristic(inputFile::String, timeLimit::Int64)
     include(inputFile)          # contains n, coordinates, lh[v], L, w_v, W_v, W, K, B,
     println("nb max de cluster : K = ", K)
@@ -121,32 +272,65 @@ function heuristic(inputFile::String, timeLimit::Int64)
     m = n*n
     l = generate_l(coordinates, n)
 
-    # does not always generate a full solution
-    sol, delta2 = genRandomSol(n, m, K, B, L, l, lh, w_v, W_v, W)
-    vectorSol = sol2Dto1D(sol)
-    val = partitionValue(vectorSol, n, m, l, lh, L)
-    println("First solution value: ", val)
-
-    w_v2 = w_v .* (delta2 .+ 1)
-
-    # generating a list of tuples of nodes with same w_v^2
-    couples = couplesWithSameW2(w_v2)
-
     start = time()
-    computationTime = time() - start
-    while computationTime < timeLimit
-        # we try to switch two nodes (which are not currently in the same cluster)
-        vectorSol2 = switchTwoNodes(vectorSol, couples)
-        val2 = partitionValue(vectorSol2, n, m, l, lh, L)
-        if val2 < val
-            val = val2
-            vectorSol = vectorSol2
-            println("After ", computationTime, " seconds, found a better solution with value ", val)
-        end
 
-        computationTime = time() - start
+    println("LOOKING FOR FIRST SOLUTION")
+    currentSol1D = genSolWithSolver(n, m, K, B, L, l, lh, w_v, W_v, W)
+    currentSol2D = sol1Dto2D(currentSol1D, K, n)
+    currentValue = partitionValue(currentSol1D, n, m, l, lh, L)
+    println("found with value: ", currentValue)
+
+    # getting the value, delta_2 and w_v2 for each cluster
+    println("GETTING USEFUL DATA ABOUT INITIAL SOLUTION")
+    delta2 = Vector()
+    w_v2 = Vector()
+    clusterValues = Vector()
+    for k in 1:K
+        clusVal, delta2Clus = clusterValue(k, currentSol2D, n, m, w_v, W_v, W)
+        push!(delta2, delta2Clus)
+        append!(clusterValues, clusVal)
+        w_v2k = w_v .* (delta2Clus .+ 1)
+        push!(w_v2, w_v2k)
     end
 
-    println("generated sol : ", vectorSol)
-    return vectorSol
+    # trying to move around vertices
+    nodeIndices = [i for i in 1:n]
+    shuffle!(nodeIndices)
+    for nodeIdx in nodeIndices
+        moved = simpleMove(nodeIdx, currentSol1D, currentSol2D, K, B, n, m, w_v, W_v, W, currentValue)
+        if moved
+            println("node ", nodeIdx, " changed of cluster")
+        end
+    end
+    currentValue = partitionValue(currentSol1D, n, m, l, lh, L)
+    println("value after search : ", currentValue)
+
+    return currentSol1D, currentValue
 end
+
+    # generating a list of tuples of nodes with same w_v^2
+    #couples = couplesWithSameW2(w_v2)
+    #println("COUPLES : ", couples)
+
+    #computationTime = time() - start
+
+    #println("STARTING LOCAL SEARCH")
+    #while computationTime < timeLimit
+    #    # we try to switch two nodes from different clusters
+    #    otherSol1D = switchTwoNodes(currentSol1D, couples)
+    #    otherValue = partitionValue(otherSol1D, n, m, l, lh, L)
+    #    if otherValue < currentValue
+    #        currentValue = otherValue
+    #        currentSol1D = otherSol1D
+    #        println("better solution after ",  computationTime, "s with value : ", currentValue)
+    #    end
+
+    #    computationTime = time() - start
+    #end
+
+#    println("generated sol : ", currentSol1D)
+#    return currentSol1D
+#end
+
+sol, val = heuristic("data/40_eil_6.tsp", 60)
+println(sol)
--- neighborhood.jl
+++ neighborhood.jl
@@ -1,18 +1,106 @@
-function removeCouplesInSameCluster(vectorSol, couples)
-    couplesToSwich = []
-    for c in couples
-        if vectorSol[c[1]] != vectorSol[c[2]]
-            push!(couplesToSwich, c)
-        end
+include("common.jl")
+
+### TO COMPUTE THE VALUE OF THE GIVEN PARTITION
+function partitionValue(vectorPartition, n::Int64, m::Int64, l, lh, L::Int64)::Float64
+    x = xFromPartition(vectorPartition, n, m)
+
+    solVal = Model(CPLEX.Optimizer)
+    @variable(solVal, delta1[e in 1:m] >= 0)
+
+    @constraint(solVal, sum(delta1[e] for e in 1:m) <= L)
+    @constraint(solVal, [e in 1:m], delta1[e] <= 3)
+
+    @objective(solVal, Max, sum(x[e]*(1/2*l[e]+delta1[e]*(lh[node1(e, n)]+lh[node2(e, n)])) for e in 1:m))
+
+    # Désactive les sorties de CPLEX (optionnel)
+    set_optimizer_attribute(solVal, "CPX_PARAM_SCRIND", 0)
+    optimize!(solVal)
+
+    feasiblefound = primal_status(solVal) == MOI.FEASIBLE_POINT
+    if feasiblefound
+        value = JuMP.objective_value(solVal)
     end
-    return couplesToSwich
+
+    return value
 end
 
-function switchTwoNodes(vectorSol, couples)
-    couplesToSwich = removeCouplesInSameCluster(vectorSol, couples)
-    randomIdx = rand(1:length(couplesToSwich))
-    c = couplesToSwich[randomIdx]
-    vectorSol[c[1]] = vectorSol[c[2]]
-    vectorSol[c[2]] = vectorSol[c[1]]
-    return vectorSol
+### TO COMPUTE THE VALUE OF CLUSTER k IN THE GIVEN PARTITION
+function clusterValue(k::Int64, partition2D, n::Int64, m::Int64, w_v, W_v, W::Int64)
+    clusVal = Model(CPLEX.Optimizer)
+    @variable(clusVal, delta2[v in 1:n] >= 0)
+
+    @constraint(clusVal, sum(delta2[v] for v in 1:n) <= W)
+    @constraint(clusVal, [v in 1:n], delta2[v] <= W_v[v])
+
+    @objective(clusVal, Max, sum(w_v[v]*partition2D[k][v]*(1+delta2[v]) for v in 1:n))
+
+    # Désactive les sorties de CPLEX (optionnel)
+    set_optimizer_attribute(clusVal, "CPX_PARAM_SCRIND", 0)
+    optimize!(clusVal)
+
+    feasiblefound = primal_status(clusVal) == MOI.FEASIBLE_POINT
+    if feasiblefound
+        delta2Star = JuMP.value.(delta2)
+        clusterVal = JuMP.objective_value(clusVal)
+    end
+
+    return clusterVal, delta2Star
+end
+
+# returns a vector with couples that are not in the same cluster
+function removeSameClusterCouples(vectorSol::Vector{Int64}, couples)
+    couplesToSwitch = Vector()
+    for c in couples
+        if vectorSol[c[1]] != vectorSol[c[2]]
+            push!(couplesToSwitch, c)
+        end
+    end
+    return couplesToSwitch
+end
+
+# swaps a couple at random, making sure both nodes are not in the same cluster
+function switchTwoNodes(vectorSol::Vector{Int64}, couples)
+    couplesToSwitch = removeSameClusterCouples(vectorSol, couples)
+    randomIdx = rand(1:length(couplesToSwitch))
+    c = couplesToSwitch[randomIdx]
+    swappedVectorSol = copy(vectorSol)
+    swappedVectorSol[c[1]] = vectorSol[c[2]]
+    swappedVectorSol[c[2]] = vectorSol[c[1]]
+    return swappedVectorSol
+end
+
+# moves node nodeIdx to another cluster if it enhances the solution
+function simpleMove(nodeIdx::Int64, sol1D::Array{Int64}, sol2D, K::Int64, B::Int64, n::Int64, m::Int64, w_v, W_v, W::Int64, currentValue::Float64)
+    originCluster = sol1D[nodeIdx]
+    # k is the cluster to put the node in
+    if originCluster==1
+        k = 2
+    else
+        k = 1
+    end
+    goOn = true
+    while k<=K && goOn
+        # test simple move
+        sol2D[k][nodeIdx] = true
+        movedClusterValue, delta2 = clusterValue(k, sol2D, n, m, w_v, W_v, W)
+        if movedClusterValue <= B
+            sol1D[nodeIdx] = k
+            movedValue = partitionValue(sol1D, n, m, l, lh, L)
+            if movedValue < currentValue
+                goOn = false
+                sol2D[originCluster][nodeIdx] = false
+            else
+                sol1D[nodeIdx] = originCluster
+                sol2D[k][nodeIdx] = false
+                sol2D[originCluster][nodeIdx] = true
+            end
+        else
+            sol2D[k][nodeIdx] = false
+        end
+        k += 1
+        if k==originCluster
+            k+=1
+        end
+    end
+    return !goOn
 end
